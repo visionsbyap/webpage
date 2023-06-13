b92eb23f4d7c212c8c9b6d26647275e641ac3840
visionsbyap <136380247+visionsbyap@users.noreply.github.com>
2023-06-13 16:53:08 -0400

    Update StoriesList.vue

--- components/content/StoriesList.vue
+++ components/content/StoriesList.vue
@@ -4,28 +4,28 @@ import { withTrailingSlash } from "ufo";
 const props = defineProps({
   path: {
     type: String,
-    default: "stories",
+    default: "events",
   },
 });
 
-const { data: _stories } = await useAsyncData(
-  "stories",
+const { data: _events } = await useAsyncData(
+  "events",
   async () =>
     await queryContent(withTrailingSlash(props.path)).sort({ date: -1 }).find()
 );
 
-const stories = computed(() => _stories.value || []);
+const events = computed(() => _events.value || []);
 </script>
 
 <template>
   <div
-    v-if="stories?.length"
+    v-if="events?.length"
     class="not-prose grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8"
   >
-    <StoryListItem v-for="story in stories" :key="story._path" :story="story" />
+    <StoryListItem v-for="eventt in events" :key="event._path" :event="events" />
     
   </div>
   <div v-else>
-    <p class="">No Stories found.</p>
+    <p class="">No Event found.</p>
   </div>
 </template>
