1bcb5ef38922baf2c5217e6818769bb7d6309b87
visionsbyap <136380247+visionsbyap@users.noreply.github.com>
2023-06-13 01:54:25 -0400

    Update Footer.vue

--- components/Footer.vue
+++ components/Footer.vue
@@ -3,7 +3,7 @@
     <div class="dark:text-zinc-500 pb-16 mt-16">
       <div class="text-center">
         <!-- copyright -->
-        <p class="text-sm">&copy; {{ new Date().getFullYear() }} <NuxtLink to="/" class="text-zinc-500 hover:text-zinc-400">Oscar Mattern</NuxtLink>, All rights reserved.</p>
+        <p class="text-sm">&copy; {{ new Date().getFullYear() }} <NuxtLink to="/" class="text-zinc-500 hover:text-zinc-400">Visions By AP</NuxtLink>, All rights reserved.</p>
       </div>
     </div>
   </Container>
