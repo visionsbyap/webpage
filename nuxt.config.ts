41e7a52df02bdbf54d5932e186b36fdb95ac21b2
visionsbyap <136380247+visionsbyap@users.noreply.github.com>
2023-06-13 11:04:35 -0400

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   pages: true,
-  "target" : "static"
-    generate: {
-    dir: 'dist'
-  }
   /**
    * Nuxt.js modules
    */
