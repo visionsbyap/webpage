0276c4da37ba1122548b21d41056d51033893012
visionsbyap <136380247+visionsbyap@users.noreply.github.com>
2023-06-13 02:04:59 -0400

    Update SectionCtaHireMe.vue

--- components/content/SectionCtaHireMe.vue
+++ components/content/SectionCtaHireMe.vue
@@ -3,31 +3,30 @@
     <div class="relative px-4 py-12 lg:py-24 rounded-2xl overflow-hidden border border-zinc-200/50 dark:border-yellow-100/5 shadow-sm dark:shadow-2xl">
       <div class="">
         <h2 class="font-display font-thin text-4xl max-w-2xl mx-auto text-center text-zinc-900 dark:text-gradient">
-          <slot name="title">Questions or collaboration ideas?</slot>
+          <slot name="title">Questions or Inquiring?</slot>
         </h2>
         <div class="flex flex-col gap-4 mt-8 max-w-lg mx-auto text-center text-zinc-600 dark:text-zinc-400 text-sm">
           <slot name="description">
-            <p>I would love to hear from you! Whether you have questions about my work, have collaboration ideas, or are interested in hiring me for a project, don't hesitate to reach out.</p>
-            <p>I usually respond within hours!</p>
+            <p>Thank you for considering Visions By AP to be a part of your special day, Tell us a little about yourself and how we can help make your day extra special.</p>
           </slot>
         </div>
         <ul class="mt-16 flex flex-col gap-4 lg:gap-12 lg:flex-row items-center justify-center max-w-2xl mx-auto">
           <li>
-            <NuxtLink to="mailto:oscar@mattern.com" class="flex items-center gap-2 text-sm text-zinc-500 hover:text-zinc-800 dark:text-zinc-400 dark:hover:text-zinc-100">
+            <NuxtLink to="mailto:info@visionsbyap.com" class="flex items-center gap-2 text-sm text-zinc-500 hover:text-zinc-800 dark:text-zinc-400 dark:hover:text-zinc-100">
               <Icon name="heroicons:envelope-20-solid" size="1.8rem" />
-              <span>oscar@mattern.com</span>
+              <span>info@visionsbyap.com</span>
             </NuxtLink>
           </li>
           <li>
-            <NuxtLink to="https://instagram.org" class="flex items-center gap-2 text-sm text-zinc-500 hover:text-zinc-800 dark:text-zinc-400 dark:hover:text-zinc-100">
+            <NuxtLink to="https://instagram.com/visionsbyap" class="flex items-center gap-2 text-sm text-zinc-500 hover:text-zinc-800 dark:text-zinc-400 dark:hover:text-zinc-100">
               <Icon name="fe:instagram" size="1.8rem" />
-              <span>oscarphotography</span>
+              <span>visionsbyap</span>
             </NuxtLink>
           </li>
           <li>
-            <NuxtLink to="https://instagram.org" class="flex items-center gap-2 text-sm text-zinc-500 hover:text-zinc-800 dark:text-zinc-400 dark:hover:text-zinc-100">
+            <NuxtLink to="https://facebook.com/visionsbyap" class="flex items-center gap-2 text-sm text-zinc-500 hover:text-zinc-800 dark:text-zinc-400 dark:hover:text-zinc-100">
               <Icon name="fe:twitter" size="1.8rem" />
-              <span>oscarphotography</span>
+              <span>visionsbyap</span>
             </NuxtLink>
           </li>
         </ul>
