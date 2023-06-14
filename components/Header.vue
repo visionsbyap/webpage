12e89a1aafe4ac67b0a88c2a6d5b00cc0f60d669
visionsbyap <136380247+visionsbyap@users.noreply.github.com>
2023-06-13 20:26:18 -0400

    Add files via upload

--- components/Header.vue
+++ components/Header.vue
@@ -1,61 +1,16 @@
-<script setup lang="ts">
-const links = [
-  { name: "Home", to: "/" },
-  { name: "Gallery", to: "/galleries" },
-  { name: "Events", to: "/events" },
-  { name: "Contact", to: "/contact" },
-  { name: "FAQ", to: "/faq" },
-] as { name: string; to: string }[];
+<script setup lang="ts"></script>
 
-onMounted(() => {
-  window.addEventListener("scroll", onScroll);
-});
-onBeforeUnmount(() => {
-  window.removeEventListener("scroll", onScroll);
-});
-
-const showHeader = ref(true);
-const lastScrollPosition = ref(0);
-
-const onScroll = () => {
-  const currentScrollPosition =
-    window.pageYOffset || document.documentElement.scrollTop;
-  // Momentum scrolling on iOS can cause the scroll position to be negative
-  if (currentScrollPosition < 0) return;
-
-  // add 60px delay
-  if (Math.abs(currentScrollPosition - lastScrollPosition.value) < 60) return;
-
-  // show if scrolling up
-  showHeader.value = currentScrollPosition < lastScrollPosition.value;
-
-  lastScrollPosition.value = currentScrollPosition;
-};
-</script>
 <template>
-  <div
-    :class="[
-      showHeader ? 'translate-y-0' : '-translate-y-full',
-      'transform-gpu transition-transform duration-500 sticky top-0 z-50',
-    ]"
-  >
-    <Container class="pt-4 lg:pt-10">
-      <div class="flex items-center justify-between">
-        <Logo />
-        <div
-          class="border border-zinc-300/50 dark:border-zinc-900/60 rounded-full pl-2 lg:pl-4 pr-2 py-2 backdrop-blur-lg bg-zinc-100/50 dark:bg-zinc-800/50"
-        >
-          <div class="flex items-center gap-4">
-            <div class="hidden lg:block">
-              <NavLinks :links="links" />
-            </div>
-            <div class="lg:hidden">
-              <NavLinksMobile :links="links" />
-            </div>
-            <ColorModeSwitch />
-          </div>
-        </div>
-      </div>
-    </Container>
-  </div>
+  <header class="flex align-items justify-between px-16 py-4 border-b-[1px] border-fg font-medium text-sm">
+    <router-link to="/forrms/" class="text-primary font-bold text-base">Forrms</router-link>
+    <nav class="flex gap-4">
+      <router-link to="/forrms/about" class="transition-colors cursor-pointer hover:text-primary">About</router-link>
+      <a href="https://github.com/antoniopataro/forrms" target="_blank" class="transition-colors hover:text-primary"
+        >Repository</a
+      >
+      <router-link to="/forrms/technologies" class="transition-colors cursor-pointer hover:text-primary"
+        >Technologies</router-link
+      >
+    </nav>
+  </header>
 </template>
