d1df9d918b4336b0b2fba5bb673a6f7288b83249
visionsbyap <136380247+visionsbyap@users.noreply.github.com>
2023-06-13 20:33:07 -0400

    Update Footer.vue

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,61 +1,10 @@
-<script setup lang="ts">
-const links = [
-  { name: "Home", to: "/" },
-  { name: "Gallery", to: "/galleries" },
-  { name: "Events", to: "/events" },
-  { name: "Contact", to: "/contact" },
-  { name: "FAQ", to: "/faq" },
-] as { name: string; to: string }[];
-
-onMounted(() => {
-  window.addEventListener("scroll", onScroll);
-});
-onBeforeUnmount(() => {
-  window.removeEventListener("scroll", onScroll);
-});
-
-const showHeader = ref(true);
-const lastScrollPosition = ref(0);
-
-const onScroll = () => {
-  const currentScrollPosition =
-    window.pageYOffset || document.documentElement.scrollTop;
-  // Momentum scrolling on iOS can cause the scroll position to be negative
-  if (currentScrollPosition < 0) return;
-
-  // add 60px delay
-  if (Math.abs(currentScrollPosition - lastScrollPosition.value) < 60) return;
-
-  // show if scrolling up
-  showHeader.value = currentScrollPosition < lastScrollPosition.value;
-
-  lastScrollPosition.value = currentScrollPosition;
-};
-</script>
 <template>
-  <div
-    :class="[
-      showHeader ? 'translate-y-0' : '-translate-y-full',
-      'transform-gpu transition-transform duration-500 sticky top-0 z-50',
-    ]"
-  >
-    <Container class="pt-4 lg:pt-10">
-      <div class="flex items-center justify-between">
-        <Logo />
-        <div
-          class="border border-zinc-300/50 dark:border-zinc-900/60 rounded-full pl-2 lg:pl-4 pr-2 py-2 backdrop-blur-lg bg-zinc-100/50 dark:bg-zinc-800/50"
-        >
-          <div class="flex items-center gap-4">
-            <div class="hidden lg:block">
-              <NavLinks :links="links" />
-            </div>
-            <div class="lg:hidden">
-              <NavLinksMobile :links="links" />
-            </div>
-            <ColorModeSwitch />
-          </div>
-        </div>
+  <Container>
+    <div class="dark:text-zinc-500 pb-16 mt-16">
+      <div class="text-center">
+        <!-- copyright -->
+        <p class="text-sm">&copy; {{ new Date().getFullYear() }} <NuxtLink to="/" class="text-zinc-500 hover:text-zinc-400">Visions By AP</NuxtLink>, All rights reserved.</p>
       </div>
-    </Container>
-  </div>
+    </div>
+  </Container>
 </template>
