52ae50d6c31c56b4ef21a61eea31441a2e29e366
visionsbyap <136380247+visionsbyap@users.noreply.github.com>
2023-06-14 09:47:23 -0400

    Update SectionFaq.vue

--- components/content/SectionFaq.vue
+++ components/content/SectionFaq.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="not-prose mt-32">
+  <div class="not-prose mt-5">
     <div class="divide-y divide-zinc-900/10 dark:divide-zinc-100/10">
       <h2 class="text-2xl font-bold leading-10 tracking-tight text-zinc-900 dark:text-zinc-100">
         <slot name="title"></slot>
