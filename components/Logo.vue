ced24cb672c2e2168d616b9c37267d31b31e0c89
visionsbyap <136380247+visionsbyap@users.noreply.github.com>
2023-06-13 23:17:31 -0400

    Update Logo.vue

--- components/Logo.vue
+++ components/Logo.vue
@@ -3,7 +3,7 @@
     to="/"
     class="group flex items-center gap-4 text-zinc-700 dark:text-zinc-400 hover:text-zinc-900 focus:outline-none focus-visible:ring-offset-2 ring-offset-zinc-900 focus:rounded-full focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
   >
-    <div class="rounded-full border-2 border-white dark:border-zinc-400 shadow-md group-hover:shadow-xl transition-shadow duration-100 shrink-0">
+    <div class="dark:border-zinc-400 shadow-md group-hover:shadow-xl transition-shadow duration-100 shrink-0">
       <NuxtImg
         class="inline-block h-14 w-14"
         src="/img/logo.jpg"
