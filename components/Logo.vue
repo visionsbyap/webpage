0419019298007770b27b7c7f755026f8db2e8424
visionsbyap <136380247+visionsbyap@users.noreply.github.com>
2023-06-14 01:59:28 -0400

    Update Logo.vue

--- components/Logo.vue
+++ components/Logo.vue
@@ -3,14 +3,14 @@
     to="/"
     class="group flex items-center gap-4 text-zinc-700 dark:text-zinc-400 hover:text-zinc-900 focus:outline-none focus-visible:ring-offset-2 ring-offset-zinc-900 focus:rounded-full focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
   >
-    <div class="dark:border-zinc-400 shadow-md group-hover:shadow-xl transition-shadow duration-100 shrink-0">
+    <div class="transition-shadow duration-100 shrink-0">
       <NuxtImg
-        class="inline-block h-14 w-14"
+        class="inline-block h-50 w-50"
         src="/img/logo.png"
         alt="Pratheep Logaranjan"
         format="webp"
-        width="100"
-        height="100"
+        width="200"
+        height="200"
       />
     </div>
     <span class="uppercase tracking-widest hidden md:inline-flex shrink-1 sr-only">Pratheep Logaranjan</span>
