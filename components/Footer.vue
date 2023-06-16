ca5a44462fdfb03e80d2cc50e01d068a9bbb370a
visionsbyap <136380247+visionsbyap@users.noreply.github.com>
2023-06-16 00:22:44 -0400

    Update Footer.vue

--- components/Footer.vue
+++ components/Footer.vue
@@ -3,7 +3,7 @@
     <div class="dark:text-zinc-500 pb-16 mt-16">
       <div class="text-center">
         <!-- copyright -->
-        <p class="text-sm">&copy; {{ new Date().getFullYear() }} <NuxtLink to="/" class="text-zinc-500 hover:text-zinc-400">Visions By AP</NuxtLink>, All rights reserved <NuxtLink to="/locked" class="text-zinc-500 hover:text-zinc-400">.</NuxtLink></p>
+        <p class="text-sm">&copy; {{ new Date().getFullYear() }} <NuxtLink to="/" class="text-zinc-500 hover:text-zinc-400">Visions By AP</NuxtLink>, All rights reserved<NuxtLink to="/locked" class="text-zinc-500 hover:text-zinc-400">.</NuxtLink></p>
       </div>
     </div>
   </Container>
